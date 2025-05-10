b83d94825013be651027bb28b4cb72f891462a92
nguyenhanh1612 <hanhnguyen016012@gmail.com>
2025-05-10 07:03:13 +0700

    completed UI contact

--- src/app/(user)/contact/components/contact.tsx
+++ src/app/(user)/contact/components/contact.tsx
@@ -1,7 +1,10 @@
+'use client'
 import React from 'react'
 import { MdLocationPin } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
+import { motion } from "framer-motion";
+import { fadeIn } from '@/utils/variants';
 
 export default function Contact() {
     return (
@@ -9,15 +12,26 @@ export default function Contact() {
             <section>
                 <div className="relative w-full h-96"><img className="absolute h-full w-full object-cover object-center" src="https://tienganhtieuhoc.edu.vn/images/home/banner.png" alt="nature image" />
                     <div className="absolute inset-0 h-full w-full bg-black/50"></div>
-                    <div className="relative pt-28 text-center">
+                    <motion.div
+                        variants={fadeIn("up", 0.3)}
+                        initial="hidden"
+                        whileInView="show"
+                        viewport={{ once: false, amount: 0.7 }}
+                        className="relative pt-28 text-center">
                         <h2 className="block antialiased tracking-normal font-sans font-semibold leading-[1.3] text-white mb-4 text-3xl lg:text-4xl mt-6">Thông tin liên hệ</h2>
                         <p className="block antialiased font-sans text-xl font-normal leading-relaxed text-white mb-9 opacity-70">Chúng tôi luôn sẵn sàng hỗ trợ bạn. Hãy liên hệ với chúng tôi nếu có bất kỳ câu hỏi nào!</p>
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="-mt-16 mb-8 px-8 ">
                     <div className="container mx-auto">
-                        <div className="py-12 flex flex-col md:flex-row justify-center gap-12 rounded-xl border border-white bg-white shadow-md shadow-black/5 saturate-200 px-4">
-                            <div className="my-4 md:my-8 grid gap-6 px-4">
+                        <div
+                            className="py-12 flex flex-col md:flex-row justify-center gap-12 rounded-xl border border-white bg-white shadow-md shadow-black/5 saturate-200 px-4">
+                            <motion.div
+                                variants={fadeIn("right", 0.3)}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{ once: false, amount: 0.7 }}
+                                className="my-4 md:my-8 grid gap-6 px-4">
                                 <div className="flex items-center gap-4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="h-6 w-6">
                                     <MdLocationPin className='text-2xl' />
                                 </svg>
@@ -33,8 +47,12 @@ export default function Contact() {
                                 </svg>
                                     <p className="block antialiased font-sans text-base leading-relaxed text-inherit font-bold">blindtreasure@gmail.com</p>
                                 </div>
-                            </div>
-                            <div className="py-4">
+                            </motion.div>
+                            <motion.div variants={fadeIn("left", 0.3)}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{ once: false, amount: 0.7 }}
+                                className="py-4">
                                 <form action="#">
                                     <div className="mb-4">
                                         <div className="relative w-full min-w-[200px] h-11 !min-w-full"><input type="text" name="Name" className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-3 rounded-md border-blue-gray-200 focus:border-gray-900" placeholder=" " /><label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[&#x27; &#x27;] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[&#x27; &#x27;] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">Nhập tên </label></div>
@@ -54,7 +72,7 @@ export default function Contact() {
                                             <p className="block antialiased font-sans text-base leading-relaxed text-inherit font-normal !text-gray-500">Bạn đồng ý với <a href="#" className="font-medium text-gray-700 hover:text-gray-900">Chính sách bảo mật</a>.</p>
                                         </label></div><button className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full mt-6" type="button">Liên hệ với chúng tôi</button>
                                 </form>
-                            </div>
+                            </motion.div>
                         </div>
                     </div>
                 </div>
