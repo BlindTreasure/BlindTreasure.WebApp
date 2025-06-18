1382c89dbf69f64d3ead9d9a6fac35a1d55361fc
nguyenhanh1612 <hanhnguyen016012@gmail.com>
2025-06-19 04:08:28 +0700

    check user

--- src/app/(user)/purchased/page.tsx
+++ src/app/(user)/purchased/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 import Purchased from "./components/purchased";
+import RequireAuth from "@/components/require-auth";
 
 export const metadata: Metadata = {
     title: "Đơn mua",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function PurchasedPage() {
     return (
         <div>
-            <Purchased />
+            <RequireAuth>
+                <Purchased />
+            </RequireAuth>
         </div>
     )
 }
